9bef486074cd1f755b84938cf9a0f409a3890015
Viunze <cellodestian5@gmail.com>
2025-07-06 02:48:47 -0700

    Create ZeHubb.lua

---
+++ ZeHubb.lua
@@ -0,0 +1,92 @@
+Scripts/Blue-lock-no-cd-reo-get-any-style-script.-WORKS-WITH-WORLD-CLASSES-/refs/heads/main/script.lua")
+addScript(Tab5, "Alchemy Hub", "https://scripts.alchemyhub.xyz")
+addScript(Tab5, "Dehism Auto Spins", "https://raw.githubusercontent.com/dehism/Dehism/refs/heads/main/Inf%20Spins%20Auto%20Dehism")
+addScript(Tab5, "Pastefy vrlg7VPE", "https://pastefy.app/vrlg7VPE/raw")
+addScript(Tab5, "Pastebin SPQT6v5J", "https://pastebin.com/raw/SPQT6v5J")
+addScript(Tab5, "Zuyi FullHub", "https://raw.githubusercontent.com/PresidentZuyi/S/refs/heads/main/FullHub")
+addScript(Tab5, "Nicuse Hub", "https://nicuse.xyz/MainHub.lua")
+addScript(Tab5, "OT Gehlee", "https://raw.githubusercontent.com/OhhMyGehlee/fo/refs/heads/main/ot")
+addScript(Tab5, "ZEEHubVN", "https://zuwz.xyz/ZEEHubVNBlueLock.lua")
+addScript(Tab5, "Omgshit Loader", "https://raw.githubusercontent.com/Omgshit/Scripts/main/MainLoader.lua")
+addScript(Tab5, "Selcia Key System", "https://raw.githubusercontent.com/selciawashere/screepts/refs/heads/main/BLRTBDKEYSYS")
+addScript(Tab5, "OverHub Keyed", "https://api.overhub.xyz/keys/script/overhub")
+addScript(Tab5, "Pastefy O3F7JYSF", "https://pastefy.app/O3F7JYSF/raw")
+addScript(Tab5, "Pastefy ZcC3BzKb", "https://pastefy.app/ZcC3BzKb/raw")
+addScript(Tab5, "Pastefy lbLVUm8Z", "https://pastefy.app/lbLVUm8Z/raw")
+addScript(Tab5, "Pastefy 5i2DScDc", "https://pastefy.app/5i2DScDc/raw")
+addScript(Tab5, "Pastefy 8nNevou3", "https://pastefy.app/8nNevou3/raw")
+addScript(Tab5, "BL Rivals 1", "https://raw.githubusercontent.com/LHking123456/jvJzb8FPs5TuR/refs/heads/main/BLRivals")
+addScript(Tab5, "BL Rivals 2", "https://raw.githubusercontent.com/LHking123456/ct4gFewpLxRHJ/refs/heads/main/BLRivals")
+addScript(Tab5, "Pastebin D1M2PLua", "https://pastebin.com/raw/D1M2PLua")
+addScript(Tab5, "Gumanba Script", "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/BlueLock")
+
+-- Arise Crossover
+addScript(Tab6, "Devry Hub", "https://raw.githubusercontent.com/defnotry/devry-hub/main/arise-crossover/main.lua")
+addScript(Tab6, "Reav Scripts", "https://raw.githubusercontent.com/reavscripts/hub/refs/heads/main/reavscripts.lua")
+addScript(Tab6, "Perfectus", "https://raw.githubusercontent.com/perfectusmim1/script/main/crossover")
+addScript(Tab6, "MainHub Nicuse", "https://nicuse.xyz/MainHub.lua")
+addScript(Tab6, "Alchemy Hub", "https://scripts.alchemyhub.xyz")
+addScript(Tab6, "Elgato", "https://raw.githubusercontent.com/meobeo8/elgato/a/Loader")
+addScript(Tab6, "SkyloLand", "https://raw.githubusercontent.com/SKOIXLL/SKYLOLAND/refs/heads/main/Load.lua")
+addScript(Tab6, "Gentle Hub", "https://raw.githubusercontent.com/GentleScriptHub/GentleHub/refs/heads/main/Games")
+addScript(Tab6, "Speed Hub", "https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua")
+addScript(Tab6, "SkyloLand (Duplicate)", "https://raw.githubusercontent.com/SKOIXLL/SKYLOLAND/refs/heads/main/Load.lua")
+addScript(Tab6, "Jamal Scripts", "https://raw.githubusercontent.com/IAmJamal10/Scripts/refs/heads/main/AriseCrossover")
+addScript(Tab6, "Gehlee Hub", "https://raw.githubusercontent.com/OhhMyGehlee/y/refs/heads/main/hj")
+addScript(Tab6, "Gumanba", "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/AriseCrossover")
+
+-- Fisch
+addScript(Tab7, "UB Loader", "https://gitlab.com/r_soft/main/-/raw/main/LoadUB.lua")
+addScript(Tab7, "Space Hub", "https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/Multi")
+addScript(Tab7, "Projekt Eta (Femscr)", "https://raw.githubusercontent.com/ProjektEta/Femscr.dll/refs/heads/main/Loader.lua")
+addScript(Tab7, "Disch Script", "https://raw.githubusercontent.com/hakariqScripts/disch2/refs/heads/main/disch%20script")
+addScript(Tab7, "Rift Loader", "https://github.com/Synergy-Networks/products/raw/main/Rift/loader.lua")
+addScript(Tab7, "Superman S6", "https://raw.githubusercontent.com/Superman245/sc2/refs/heads/main/s6")
+addScript(Tab7, "Luarmor Loader A", "https://api.luarmor.net/files/v3/loaders/a3c501e721ec4d66a864cc2276c0f4e9.lua")
+addScript(Tab7, "Luarmor Loader B", "https://api.luarmor.net/files/v3/loaders/cba17b913ee63c7bfdbb9301e2d87c8b.lua")
+addScript(Tab7, "YHUB Fisch", "https://raw.githubusercontent.com/Luarmor123/community-Y-HUB/refs/heads/main/Fisch-YHUB")
+addScript(Tab7, "Solix New KeyUI", "https://raw.githubusercontent.com/debunked69/Solixreworkkeysystem/refs/heads/main/solix%20new%20keyui.lua")
+addScript(Tab7, "RaitoHub", "https://raw.githubusercontent.com/Efe0626/RaitoHub/refs/heads/main/Script")
+addScript(Tab7, "Luarmor Loader C", "https://api.luarmor.net/files/v3/loaders/2529a5f9dfddd5523ca4e22f21cceffa.lua")
+addScript(Tab7, "KiciaHook", "https://raw.githubusercontent.com/kiciahook/kiciahook/refs/heads/main/loader.lua")
+addScript(Tab7, "Kncrypt Fisch", "https://raw.githubusercontent.com/3345-c-a-t-s-u-s/Kncrypt/refs/heads/main/sources/Fisch.lua")
+addScript(Tab7, "Naoki Hub", "https://naokihub.vercel.app")
+addScript(Tab7, "Londnee Fisch", "https://raw.githubusercontent.com/londnee/code/refs/heads/main/Fisch.lua")
+addScript(Tab7, "DENGHUB WL", "https://raw.githubusercontent.com/DENGHUB2025/HUGHUB/main/WL")
+addScript(Tab7, "Alchemy Hub", "https://scripts.alchemyhub.xyz")
+addScript(Tab7, "ONYX HUB", "https://raw.githubusercontent.com/zenzon23/ONYX-HUB123/refs/heads/main/FISCH")
+addScript(Tab7, "MainHub Nicuse", "https://nicuse.xyz/MainHub.lua")
+addScript(Tab7, "EternalHub Fisch", "https://raw.githubusercontent.com/Vixx77/Eternal/main/EternalHub_Fish")
+addScript(Tab7, "Kidxnox F", "https://raw.githubusercontent.com/kidxnox/f/refs/heads/main/f")
+addScript(Tab7, "Forge Hub", "https://raw.githubusercontent.com/Skzuppy/forge-hub/main/loader.lua")
+addScript(Tab7, "Femboyware", "https://raw.githubusercontent.com/kylosilly/femboyware/refs/heads/main/Fisch.lua")
+addScript(Tab7, "LOLking Fisch", "https://raw.githubusercontent.com/LOLking123456/Fisch/refs/heads/main/New")
+
+-- Steal A Brainrot
+addScript(Tab8, "AV Loader", "https://raw.githubusercontent.com/Ayvathion/AV-On-Top/refs/heads/main/Loader.lua")
+addScript(Tab8, "HFx6faQY Pastebin", "https://pastebin.com/raw/HFx6faQY")
+addScript(Tab8, "Qiwikox12 Stubrawl", "https://raw.githubusercontent.com/Qiwikox12/stubrawl/refs/heads/main/StealaBrainrot.txt")
+addScript(Tab8, "Overflow", "https://raw.githubusercontent.com/OverflowBGSI/Overflow/refs/heads/main/loader.txt")
+addScript(Tab8, "Protected OnlineGamer", "https://raw.githubusercontent.com/onliengamerop/Steal-a-brainrot/refs/heads/main/Protected_3771863424757750.lua.txt")
+addScript(Tab8, "Pastebin 8xPiNRhB", "https://pastebin.com/raw/8xPiNRhB")
+addScript(Tab8, "NEOXHUB", "https://raw.githubusercontent.com/hassanxzayn-lua/NEOXHUBMAIN/refs/heads/main/StealABrainrot")
+addScript(Tab8, "x2zu Loader", "https://raw.githubusercontent.com/x2zu/loader/main/scripts.lua")
+addScript(Tab8, "Pastefy Brainrot", "https://pastefy.app/0CVCLFPZ/raw")
+addScript(Tab8, "Gumanba MOD", "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/StealaBrainrotMOD")
+addScript(Tab8, "Chilli Spicy", "https://raw.githubusercontent.com/tienkhanh1/spicy/main/Chilli.lua")
+addScript(Tab8, "LegendHub", "https://scripts.city/LegendHub.lua")
+addScript(Tab8, "Gumanba Original", "https://raw.githubusercontent.com/gumanba/Scripts/refs/heads/main/StealaBrainrot")
+
+-- Notification
+Rayfield:Notify({
+Title = "ZeHubb GUI Loaded!",
+Content = "All scripts loaded into tabs!",
+Duration = 8,
+Image = 4483362458,
+Actions = {
+Ignore = {
+Name = "OK",
+Callback = function() end
+}
+}
+})
